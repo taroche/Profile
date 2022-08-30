21279dcbb0e61533a956a733564ae8d531691da8
Tristan <laroche11235@gmail.com>
2022-08-30 15:16:36 -0400

    update text for second slide

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -34,8 +34,25 @@ export const Slideshow = () => {
           <img className="d-block w-100" src={weather} alt="Second slide" />
 
           <Carousel.Caption className="carousel-caption-positon">
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>
+              This website allows you to check the weather anywhere in the
+              United States. You write the city for whom's weather you would
+              like in the search input and then press the button. That makes a
+              city button that when you press on it it shows you the weather for
+              today and a five day forcast. You can have multiable city buttons
+              and you can change bewteen them by click on a different city. The
+              city button will save when you reload the page.
+              <br></br>
+              Go{" "}
+              <a
+                href="https://taroche.github.io/Weather-Dashboard/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>{" "}
+              to view it.
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
