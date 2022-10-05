f84d0635fc8ec0a8ad58cfd25080756dae3345a9
Tristan <laroche11235@gmail.com>
2022-10-05 12:44:09 -0400

    added class to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,36 @@ import { Container } from "react-bootstrap";
 import { Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 
+function Footer() {
+  return (
+    <footer class="footer">
+      <Container fluid={true}>
+        <Row className="border-top justify-content-between align-items-center p-3">
+          <Col className="p-0 md={3} sm={12}">
+            <a
+              href="https://www.linkedin.com/in/tristan-laroche/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              My Linkedin
+            </a>
+            <br></br>
 
-function Footer(){
-
-    return(
-        <footer>
-            <Container fluid={true}>
-                <Row className="border-top justify-content-between align-items-center p-3">
-                    <Col className="p-0 md={3} sm={12}">
-                        <a href="https://www.linkedin.com/in/tristan-laroche/" target="_blank" rel="noreferrer">My Linkedin</a><br></br>
-
-                        <a href="https://github.com/taroche" target="_blank" rel="noreferrer">My Github</a>
-                    </Col>
-                    <Col className="p-0 d-flex justify-content-end font-weight-light md={3}">
-                        This site was made by Tristan LaRoche.
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    )
+            <a
+              href="https://github.com/taroche"
+              target="_blank"
+              rel="noreferrer"
+            >
+              My Github
+            </a>
+          </Col>
+          <Col className="p-0 d-flex justify-content-end font-weight-light md={3}">
+            This site was made by Tristan LaRoche.
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
