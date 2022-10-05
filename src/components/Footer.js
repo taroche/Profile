80c17bdd1d5ec7bc44b416599668e2664da5f182
Tristan <laroche11235@gmail.com>
2022-10-05 14:14:25 -0400

    added div so footer doesn't sit on top of text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,33 +5,36 @@ import { Col } from "react-bootstrap";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <Container fluid={true}>
-        <Row className="border-top justify-content-between align-items-center p-3">
-          <Col className="p-0 md={3} sm={12}">
-            <a
-              href="https://www.linkedin.com/in/tristan-laroche/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              My Linkedin
-            </a>
-            <br></br>
+    <>
+      <div className="push"></div>
+      <footer className="footer">
+        <Container fluid={true}>
+          <Row className="border-top justify-content-between align-items-center p-3">
+            <Col className="p-0 md={3} sm={12}">
+              <a
+                href="https://www.linkedin.com/in/tristan-laroche/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                My Linkedin
+              </a>
+              <br></br>
 
-            <a
-              href="https://github.com/taroche"
-              target="_blank"
-              rel="noreferrer"
-            >
-              My Github
-            </a>
-          </Col>
-          <Col className="p-0 d-flex justify-content-end font-weight-light md={3}">
-            This site was made by Tristan LaRoche.
-          </Col>
-        </Row>
-      </Container>
-    </footer>
+              <a
+                href="https://github.com/taroche"
+                target="_blank"
+                rel="noreferrer"
+              >
+                My Github
+              </a>
+            </Col>
+            <Col className="p-0 d-flex justify-content-end font-weight-light md={3}">
+              This site was made by Tristan LaRoche.
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
   );
 }
 
