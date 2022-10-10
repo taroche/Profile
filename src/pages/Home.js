9d27c4406a8169cc0467f16add4a5da8f75a2b08
Tristan <laroche11235@gmail.com>
2022-10-10 11:27:13 -0400

    style for the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,36 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import profilePicture from "../img/profile-picture.png";
 
 export const Home = () => {
   return (
     <>
-      <h1>HelloWorld, my name is Tristan</h1>
-      <p>Web Developer</p>
+      <div className="hello">
+        <Container>
+          <Row>
+            <Col md={6} sm={12} className="text-light">
+              <h1>HelloWorld, my name is Tristan</h1>
+              <p>Web Developer</p>
+              <p>
+                Thanks for coming to my page to checkout some of my better
+                projects and learn a little bit about me.
+              </p>
+              <div className="firework"></div>
+              <div className="firework"></div>
+              <div className="firework"></div>
+            </Col>
+            <Col md={6} sm={12}>
+              <img
+                src={profilePicture}
+                alt="profile"
+                className="profilePicture"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
