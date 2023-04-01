533a9febe8636e0169b2fb90d3ccb77ff49dec9b
Tristan LaRoche <laroche11235@gmail.com>
2023-04-01 11:59:53 -0400

    got rid of bad slide

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import noteTaker from "../img/Note_taker_homepage.png";
+
 import directory from "../img/employee-directory-image.png";
 import weather from "../img/Weather-dashboard.png";
 import beerPicker from "../img/DraughtPicker.png";
@@ -9,27 +9,7 @@ export const Slideshow = () => {
   return (
     <div className="carousel-container">
       <Carousel className="carousel" variant="dark">
-        <Carousel.Item>
-          <img className="d-block w-100" src={noteTaker} alt="First slide" />
-          <Carousel.Caption className="carousel-caption-positon">
-            <h3>Note Taker App</h3>
-            <p>
-              This is an app that lets you make notes and save them to a list.
-              The front end was provided for me and I made the database with
-              express and uuid.
-              <br></br>
-              Go{" "}
-              <a
-                href="https://sleepy-thicket-78623.herokuapp.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                here
-              </a>{" "}
-              to view it.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        
         <Carousel.Item>
           <img className="d-block w-100" src={weather} alt="Second slide" />
 
