394fc5e2399c43187a879842c0612bebc4bfe264
Tristan LaRoche <laroche11235@gmail.com>
2023-04-09 17:08:32 -0400

    function to executed httprequest on state change

--- src/components/FunFact.js
+++ src/components/FunFact.js
@@ -2,6 +2,21 @@ import React from "react";
 
 const xhr = new XMLHttpRequest();
 
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        const data = xhr.responseText;
+        const funFact = data.charAt(0).toUpperCase() + data.slice(1);
+        document.getElementById('number').textContent = funFact;
+      } else {
+        console.log('Error: ' + xhr.status);
+      }
+    }
+  };
+
+
+
 const FunFact = () => {
     return (
       <div>
