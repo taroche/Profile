a6f0bc17f9935a7b590ac2e4010b485738005461
Tristan LaRoche <laroche11235@gmail.com>
2023-04-09 16:56:16 -0400

    made fun component

---
+++ src/components/FunFact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FunFact = () => {
+    return (
+      <div>
+        <p>Fun Fact about today</p>
+        <p id="number"></p>
+      </div>
+    );
+  };
+
+  export default FunFact;
