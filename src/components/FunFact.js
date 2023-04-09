9c26a322088df8c635ea9f5bf7222caec2582983
Tristan LaRoche <laroche11235@gmail.com>
2023-04-09 17:26:57 -0400

    fix error not happen on pageLoad with useEffect

--- src/components/FunFact.js
+++ src/components/FunFact.js
@@ -1,32 +1,36 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 
-const xhr = new XMLHttpRequest();
-const d = new Date();
-const m = d.getMonth();
-const day = d.getDate();
+function FunFact() {
+  const [funFact, setFunFact] = useState('');
 
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        const data = xhr.responseText;
-        const funFact = data.charAt(0).toUpperCase() + data.slice(1);
-        document.getElementById('number').textContent = funFact;
-      } else {
-        console.log('Error: ' + xhr.status);
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    const d = new Date();
+    const m = d.getMonth();
+    const day = d.getDate();
+
+    xhr.onload = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          const data = xhr.responseText;
+          const funFactCap = data.charAt(0).toUpperCase() + data.slice(1);
+          setFunFact(funFactCap);
+        } else {
+          console.log('Error: ' + xhr.status);
+        }
       }
-    }
-  };
+    };
 
-  xhr.open('GET', `http://numbersapi.com/${day}/${m}/date?notfound=floor&fragment`, true);
-  xhr.send();
+    xhr.open('GET', `http://numbersapi.com/${day}/${m}/date?notfound=floor&fragment`, true);
+    xhr.send();
+  }, []);
 
-const FunFact = () => {
-    return (
-      <div>
-        <p>Fun Fact about today</p>
-        <p id="number"></p>
-      </div>
-    );
-  };
+  return (
+    <div>
+      <p>{funFact}</p>
+    </div>
+  );
+}
 
   export default FunFact;
